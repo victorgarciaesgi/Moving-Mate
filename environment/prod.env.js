7285fafc0005604ee3882a0f12f1b65897b32c25
Victor Garcia <victorgarciaparis13@gmail.com>
2018-07-16 19:24:35 +0200

    url prod

--- environment/prod.env.js
+++ environment/prod.env.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: '"production"',
   DEBUG_MODE: "'false'",
   API_KEY: '"XXXX-XXXXX-XXXX-XXXX"',
-  API_URL: '"http://localhost:8000/api/"'
-  // API_URL: '"http://moving.xlwlx.fr/api/"'
+  // API_URL: '"http://localhost:8000/api/"'
+  API_URL: '"http://moving.xlwlx.fr/api/"'
 }
